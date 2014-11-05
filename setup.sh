7fd5b3cd70abecbd034c2a8f158c4ac39539a14b
Allen Wu <allenwu1018@gmail.com>
2014-11-05 13:26:07 +0800

    fix some outdated packages

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 ### install prerequisites
 sudo apt-get update
-sudo apt-get install -y wget vim build-essential openssl openssl-server libreadline6 libreadline6-dev libsqlite3-dev libmysqlclient-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libxml2-dev libxslt-dev autoconf automake libtool imagemagick libmagickwand-dev libpcre3-dev language-pack-zh-hans python-software-properties
+sudo apt-get install -y wget vim build-essential openssl libreadline6 libreadline6-dev libsqlite3-dev libmysqlclient-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libxml2-dev libxslt1-dev autoconf automake libtool imagemagick libmagickwand-dev libpcre3-dev language-pack-zh-hans python-software-properties
 
 ### install nginx
 sudo apt-get install -y nginx
