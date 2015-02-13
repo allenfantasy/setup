8023c08278eb7b46b8f9f3ba7029030f411da987
Allen Wu <allenwu1018@gmail.com>
2015-02-13 17:32:47 +0800

    build files into /output

--- build-file.js
+++ build-file.js
@@ -9,15 +9,15 @@ template = fs.readFileSync('templates/nginx-config').toString();
 template = template.replace(new RegExp("%PORT%", "g"), config.app_port);
 template = template.replace(new RegExp("%APP_NAME%", "g"), config.app_name);
 template = template.replace(new RegExp("%PUBLIC_PATH%", "g"), config.public_path);
-fs.writeFileSync(config.app_name, template);
+fs.writeFileSync('output/' + config.app_name, template);
 
 // nginx deploy script
 template = fs.readFileSync('templates/nginx.sh').toString();
 template = template.replace(new RegExp("%APP_NAME%", "g"), config.app_name);
-fs.writeFileSync('nginx.sh', template);
+fs.writeFileSync('output/nginx.sh', template);
 
 // post-receive hook
 template = fs.readFileSync('templates/post-receive').toString();
 template = template.replace(new RegExp("%APP_PATH%", "g"), config.app_path);
 template = template.replace(new RegExp("%APP_NAME%", "g"), config.app_name);
-fs.writeFileSync('post-receive', template);
+fs.writeFileSync('output/post-receive', template);
